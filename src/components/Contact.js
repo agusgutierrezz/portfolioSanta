43ca2d1460fb3f88755d4dcb5f27ac8db2c98c4e
agusgutierrezz <agusgutierrezperez@gmail.com>
2020-11-29 12:08:23 +0100

    done

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import linkedin from "../assets/linkedin.svg";
+import dribble from "../assets/dribbeln.svg";
+import mail from "../assets/google-mail.svg";
+function Contact(props) {
+  return (
+    <div className="contact">
+      <h1>Contact me!</h1>
+    </div>
+  );
+}
+
+export default Contact;
